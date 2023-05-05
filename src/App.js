15b169d259c643890553a7e1b3a3d72c8e4b29dd
Akhileswar <akhileswarsathivada@gmail.com>
2023-05-05 19:10:15 +0530

    commit 1

--- src/App.js
+++ src/App.js
@@ -6,13 +6,11 @@ import SignUp from "./Pages/SignUp/SignUp";
 
 function App() {
   return (
-    <div className="app">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<SignUp />} />
+    </Routes>
   );
 }
 
